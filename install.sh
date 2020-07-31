ba0ff1a51a672f5ec54e9d02252fb3a321c121b4
keenwolf <37067814+keen-wolf@users.noreply.github.com>
2020-07-31 10:16:36 +0800

    fix wrong path

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #Just copy this cmd line and run in shell
-rpm -e `rpm -qa dstat` && cd zstat && tar -zxvf dstat-master.gz; cd dstat-master/ && make install && cd .. && cp bin/* /usr/bin && echo 'zstat installed !'
+rpm -e `rpm -qa dstat` && tar -zxvf dstat-master.gz; cd dstat-master/ && make install && cd .. && cp bin/* /usr/bin && echo 'zstat installed !'
 
